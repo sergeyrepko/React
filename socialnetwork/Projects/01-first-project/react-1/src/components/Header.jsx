a9db6ee78fbb9be1106b5a8721795c1a0cb91c6a
sergeyrepko <sergey.repko@vortexads.com>
2021-03-31 19:03:49 +0300

    added new file socialnetwork

---
+++ socialnetwork/Projects/01-first-project/react-1/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://cdn.logo.com/hotlink-ok/logo-social.png'></img>
+  </header>
+}
+
+export default Header;
---
+++ socialnetwork/Projects/01-first-project/react-1/src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+  <div>
+    <img src='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg'></img>
+  </div>
+  <div>
+  Ava + description
+  <img src='https://www.wpexplorer.com/wp-content/uploads/wordpress-image-optimization-guide.jpg'></img>
+</div>
+<div>
+      My posts
+  <div>
+        New post
+  </div>
+</div>
+<div>
+      post 1
+</div>
+<div>
+      post 2
+  </div>
+  </div>
+}
+
+export default Profile;
